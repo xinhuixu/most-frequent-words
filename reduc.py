8d2385f1b2ca4882e77a54adcf58139f7ba00bd1
xinhuixu <x.xu.1102@gmail.com>
2017-05-02 22:18:55 -0400

    1 & 2

--- reduc.py
+++ reduc.py
@@ -1,7 +1,18 @@
 f = open('warandpeace.txt', 'r')
-l = f.read()
+l = f.read().split()
 f.close()
 
+#filter creates list of ele for which a func returns TRUE.
 
 def freq(word, l):
-    return len(filter(lambda w: w==word, l.split()))
+    return len(filter(lambda w: w==word, l))
+
+def g_freq(words, l):
+    return len(filter(lambda w: w in words, l))
+
+print "freq('Andrew', l):", freq('Andrew', l)
+print "freq('war', l):", freq('war', l)
+
+group = ['war','and','peace']
+print "g_freq(['war','and','peace'], l):", g_freq(group, l)
+
